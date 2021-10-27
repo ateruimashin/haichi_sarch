34483bd8074b1e838bea1b09506e6d812c1682d2
collagePC <ateruimashin2go@gmail.com>
2021-10-27 15:29:24 +0900

    テストコードを削除

--- haich_sarch_gas/src/lastTime.js
+++
@@ -1,7 +0,0 @@
-function getLastTime() {
-  //現在の日時を取得
-  var date = Utilities.formatDate(new Date(),"Asia/Tokyo","yyyy/MM/dd HH:mm:ss");
-  PropertiesService.getScriptProperties().setProperty("date",date);
-  var value = PropertiesService.getScriptProperties().getProperty("date");
-  return value;
-}
